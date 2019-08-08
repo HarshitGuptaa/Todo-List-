d3c06da0eb672841b3f535473f5636f2b7e76d56
Harshit <gupta.harshit99@gmail.com>
2019-08-08 18:30:06 +0530

    first commit

---
+++ Assets/JS/todos.js
@@ -0,0 +1,26 @@
+//Check off todos by clicking
+$("ul").on("click", "li",function(){
+	$(this).toggleClass("completed");
+
+});
+
+
+//click on X to delete todo
+$("ul").on("click", "span",function(event){
+	$(this).parent().fadeOut(function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fas fa-trash'></i></span>" + todoText + "</li>")
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
